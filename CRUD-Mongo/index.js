15642bd6ceb07ec8fba5b12b0afbaa9b45b2c590
AmanSingh44 <aman7474362@gmail.com>
2023-06-22 20:52:05 +0545

    added .env

--- CRUD-Mongo/index.js
+++ CRUD-Mongo/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./routes/user-routes')
+require("dotenv").config();
 const app = express();
 
 app.use(express.json())
 app.use("/users", router)
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.32s6ewm.mongodb.net/")
-    .then(() => app.listen(5001, () => console.log("Database connected sucessfully")))
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.32s6ewm.mongodb.net/`)
+    .then(() => app.listen(process.env.PORT, () => console.log("Database connected sucessfully")))
     .catch((err) => console.log(err));
